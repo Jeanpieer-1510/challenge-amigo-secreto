5d0596c6a7be97d6e9fa383fc429832e9bf6ae01
Jeanpieer-1510 <jeanpieer.pb@gmail.com>
2025-01-27 12:53:26 -0500

    Se implemento la primera función para añadir los nombres al array limpiando los campos a la vez.

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-console.log("Hola Mundo")
+let listaAmigos = [];
+
+//Luego agregar la funcion de verificar si el nombre esta repetido en la lista
+function sorteoDeAmigos() {
+    const nombreAmigos = document.getElementById('amigo').value.trim();
+    if (nombreAmigos === ''){
+        alert("Por favor, inserte un nombre");
+    }else if (listaAmigos.includes(nombreAmigos)){
+        alert("El nombre ya esta en la lista");
+        limpiarCaja();
+    }else{
+        listaAmigos.push(nombreAmigos);
+        limpiarCaja();
+        console.log(nombreAmigos);
+    }
+}
+
+function limpiarCaja() {
+    return document.getElementById('amigo').value='';
+}
