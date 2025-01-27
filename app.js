43d0f328396e672f91c69c22fca0598bd945bb4b
Jeanpieer-1510 <jeanpieer.pb@gmail.com>
2025-01-27 17:38:15 -0500

    Se creo la segunda función para mostrar los nombres ingresados al array en el <ul> creando un <li> cada vez y limpiar el ul para no tener errores

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let listaDeAmigos = [];
 
-//Luego agregar la funcion de verificar si el nombre esta repetido en la lista
+//Esta función añade los nombres al arreglo despues de verificar si no esta repetido o el cuadro de texto este vacio
 function anadirAmigos() {
-    const nombreAmigos = document.getElementById('amigo').value.trim();
+    const nombreAmigos = document.getElementById('amigo').value.trim(); //.trim() es una funcion integrada que solo con llamarla es suficiente y esta elimina espacio innecesarios al inicio y al final de una oración
     if (nombreAmigos === ''){
         alert("Por favor, inserte un nombre");
-    }else if (listaAmigos.includes(nombreAmigos)){
+    }else if (listaDeAmigos.includes(nombreAmigos)){ //El metodo ".includes" se encarga de verificar la existencia de un elemento en un array o cadena de texto y da como resultado un booleano
         alert("El nombre ya esta en la lista");
         limpiarCaja();
     }else{
-        listaAmigos.push(nombreAmigos);
+        listaDeAmigos.push(nombreAmigos); //El metodo ".push" inserta o empuja el nombre a la lista
         limpiarCaja();
-        console.log(nombreAmigos);
+        actualizarLista();
+        console.log(listaDeAmigos);
     }
 }
 
+//Esta función solo limpia el input, es decir, la caja de texto, para poder seguir ingresando nombres y evitar estar borrando cada vez
 function limpiarCaja() {
     return document.getElementById('amigo').value='';
+}
+
+// Función para actualizar la lista de amigos en el HTML
+function actualizarLista() {
+    const lista = document.getElementById('listaAmigos'); // Obtener el elemento de la lista
+    lista.innerHTML = ""; // Limpiar la lista existente
+
+    // Iterar sobre el arreglo listaAmigos y crear elementos <li>
+    for (let amigo of listaDeAmigos) {
+        const li = document.createElement('li'); // Crear un nuevo elemento <li>
+        li.textContent = amigo; // Establecer el texto del <li> con el nombre del amigo
+        lista.appendChild(li); // Agregar el <li> a la lista
+    }
 }
